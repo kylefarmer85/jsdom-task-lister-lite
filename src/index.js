9fd9697f8fd2e0c917c1438bf112d1220249bc66
kylefarmer85 <kylefarmer85@gmail.com>
2020-10-27 17:16:08 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-// your code here
+
 const form = document.getElementById("create-task-form")
 const deleteButton = document.createElement("button")
 const tasks = document.getElementById("tasks")
 
 form.addEventListener("submit", function(e) {
-  //const node = document.createTextNode(newTask) 
-  //const tasks = document.getElementById("tasks")
   const newTask = e.target["new-task-description"].value 
   form.reset()
   const li = document.createElement("li")
-  //li.appendChild(node)
   li.innerHTML += `${newTask} 
   <button data-action = "delete" > X </button>` 
   tasks.appendChild(li)
-  debugger
   e.preventDefault();
 })
 
@@ -21,4 +17,4 @@ tasks.addEventListener("click", function (e) {
   if (e.target.dataset.action === "delete") {
     e.target.parentElement.remove();
   }
-});
+})
